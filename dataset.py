993c1f75414cab82989e8f892db349c5434e864f
yutake27 <TAKEI@takeiyuma-no-MacBook-Air.local>
2019-09-24 17:35:25 +0900

    add support for chainermn

--- dataset.py
+++ dataset.py
@@ -3,12 +3,12 @@ import chainer
 import os
 from PIL import Image
 
-class YuiDataset(chainer.dataset.DatasetMixin):
+class ImageDataset(chainer.dataset.DatasetMixin):
     def __init__(self, directory, depth):
         self.directory = directory
         self.files = os.listdir(directory)
         self.depth = depth
-        
+
     def __len__(self):
         return len(self.files)
 
@@ -17,7 +17,7 @@ class YuiDataset(chainer.dataset.DatasetMixin):
         t = np.random.randint(0, d+1)
         r = size[0] - (d-l)
         b = size[1] - (d-t)
-        return (l, t, r, b) 
+        return (l, t, r, b)
 
     def get_example(self, i):
         img = Image.open(os.path.join(self.directory, self.files[i]))
@@ -25,11 +25,11 @@ class YuiDataset(chainer.dataset.DatasetMixin):
 
         size = 2**(2+self.depth)
         img = img.resize((size, size))
-        
+
         img = np.array(img, dtype=np.float32) / 256
         if len(img.shape) == 2:
             img = np.broadcast_to(img, (3, img.shape[0], img.shape[1]))
         else:
             img = np.transpose(img, (2, 0, 1))
-        
+
         return img
--- generate.py
+++ generate.py
@@ -1,6 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding:utf-8 -*-
-
 import numpy as np
 import argparse
 import os
@@ -12,39 +9,50 @@ from chainer import cuda
 import dataset
 import network
 import utils
-    
+from pathlib import Path
+
 def generate():
     parser = argparse.ArgumentParser()
     parser.add_argument('--gpu', '-g', type=int, default=-1)
-    parser.add_argument('--gen', type=str, default=None)
+    parser.add_argument('--gen', type=str, default=None, help='input generate model path')
     parser.add_argument('--depth', '-d', type=int, default=0)
-    parser.add_argument('--out', '-o', type=str, default='img/')
+    parser.add_argument('--out', '-o', type=str, default='gen_img')
     parser.add_argument('--num', '-n', type=int, default=10)
+    parser.add_argument('--vector_ope', '-v', nargs=2, type=str, help='input 2 npz path')
     args = parser.parse_args()
-    
+
     gen = network.Generator(depth=args.depth)
     print('loading generator model from ' + args.gen)
     serializers.load_npz(args.gen, gen)
+
+    out_dir = Path(args.out)
+    out_dir.mkdir(exist_ok=True)
 
     if args.gpu >= 0:
         cuda.get_device_from_id(0).use()
         gen.to_gpu()
 
     xp = gen.xp
-        
-    z1 = gen.z(1)
-    z2 = gen.z(1)
+
+    if args.vector_ope:
+        print('load vector from {}'.format(args.vector_ope))
+        z1 = np.load(args.vector_ope[0])
+        z2 = np.load(args.vector_ope[1])
+    else:
+        z1 = gen.z(1)
+        z2 = gen.z(1)
 
     for i in range(args.num):
         print(i)
         p = i / (args.num-1)
-        z = z1 * p + z2 * (1 - p)
+        z = z1*(1-p) + z2*p
         x = gen(z, alpha=1.0)
         x = chainer.cuda.to_cpu(x.data)
-        
+
         img = x[0].copy()
-        filename = os.path.join(args.out, 'gen_%04d.png'%i)
+        filename = out_dir/'gen_{:04d}.png'.format(i)
         utils.save_image(img, filename)
+        np.save(filename.with_suffix('.npy'), z)
 
 if __name__ == '__main__':
     generate()
